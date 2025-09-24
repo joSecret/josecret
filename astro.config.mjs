0f3ecea2171e75a84c353dc404ec089ce3afbd83
Ion Obreja <jo@da.md>
2025-09-24 19:13:12 +0300

    Update site configuration in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,23 @@
 // @ts-check
 import {defineConfig} from 'astro/config';
+
 import mdx from "@astrojs/mdx";
-
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
-
 import icon from "astro-icon";
-
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://simple-portfolio.vicbox.dev/',
-    integrations: [mdx(), react(), tailwind({
-        applyBaseStyles: false,
-    }), icon(), sitemap()],
-    markdown: {
-        shikiConfig: {
-            theme: 'plastic',
-            wrap: true,
-        },
-    }
+  site: 'https://josecret.github.io/',
+  base: '/josecret/',
+  integrations: [mdx(), react(), tailwind({
+      applyBaseStyles: false,
+  }), icon(), sitemap()],
+  markdown: {
+    shikiConfig: {
+      theme: 'plastic',
+      wrap: true,
+    },
+  }
 });
