6ff1f514cba2345c686d7e25c902e6ea3c0a6188
Ion Obreja <jo@da.md>
2025-09-24 19:11:46 +0300

    Update site URL and base path in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://josecret.com',
+  site: 'https://josecret.github.io/',
+  base: '/josecret/',
   integrations: [tailwind(), mdx(), sitemap()],
 });
